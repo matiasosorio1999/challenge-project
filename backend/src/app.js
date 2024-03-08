d6612a3017e0e65fbde258e76e5721b41eaa12e9
Matias Osorio <matiasosorio1999@gmail.com>
2024-03-08 16:44:26 -0300

    add cors to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const issuesRouter = require('./api/issues/issues.router');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/issues', issuesRouter);

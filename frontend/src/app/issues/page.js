ceff1a4de0e96d68b247f7a1736d002d8da5b99e
Matias Osorio <matiasosorio1999@gmail.com>
2024-03-08 16:52:43 -0300

    add button to delete issues

--- frontend/src/app/issues/page.js
+++ frontend/src/app/issues/page.js
@@ -21,6 +21,20 @@ const Issues = () => {
     fetchIssues();
   }, []);
 
+  const deleteIssue = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/issues/${id}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    setIssues(issues.filter((issue) => issue.id !== id));
+  } catch (error) {
+    console.error("Could not delete issue", error);
+  }
+};
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
@@ -29,6 +43,7 @@ const Issues = () => {
           <div key={issue.id}className="border border-white w-48 p-4">
             <h2>{issue.title}</h2>
             <p>{issue.description}</p>
+            <button onClick={() => deleteIssue(issue.id)} className="p-1 bg-red-700">Delete</button>
           </div>
         ))}
       </div>
